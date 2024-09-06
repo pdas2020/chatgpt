53ab15cb8e2059de045d20c7fe08ebd57c57ac87
Prasant Das <pdas2006@gmail.com>
2024-09-05 20:03:20 -0400

    duplicate file

--- app.py
+++ app.py
@@ -2,20 +2,22 @@ from openai import OpenAI
 import streamlit as st
 import os
 
-st.title("ChatGPT-Ask me Anything")
+st.title("AIBot-Ask me Anything")
 
 #client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key=os.environ.get("OPENAI_API_KEY"),
-)
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
+
+#initialize session variable at the start
+st.sidebar.title =("Model Parameters")
+temperature = st.sidebar.slider("Temperature",min_value=0.0, max_value=2.0,value=0.7,step=0.1 )
+max_token = st.sidebar.slider("Max Token",min_value=1, max_value=1000,value=256 )    
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
